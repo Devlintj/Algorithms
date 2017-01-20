c47a9442a099aa7652d928257bbd60099a225cbc
Tyler James Devlin <devlintj@dcsdk12.org>
2017-01-20 12:56:29 -0700

    shuffle

--- src/Shuffle.java
+++ src/Shuffle.java
@@ -1,4 +1,15 @@
+import java.util.Random;
+
 
 public class Shuffle {
-
+	public static void main(String[] args){
+		int[] deck = new int[]{1, 2, 3, 4, 5, 6};
+		
+	}
+	
+	public static void shuffleMethod(int[] deck) {
+		for(int i = 0; i < deck.length; i++) {
+			Random index = new Random(deck.length);
+		}
+	}
 }
