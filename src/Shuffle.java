6f837e0467ebb3afcf69939dbcd8b459534b99e9
Tyler James Devlin <devlintj@dcsdk12.org>
2017-01-24 12:13:27 -0700

    shuffle complete

--- src/Shuffle.java
+++ src/Shuffle.java
@@ -1,15 +1,21 @@
 import java.util.Random;
-
+import java.util.Arrays;
 
 public class Shuffle {
 	public static void main(String[] args){
 		int[] deck = new int[]{1, 2, 3, 4, 5, 6};
-		
+		System.out.println(Arrays.toString(shuffleMethod(deck)));
 	}
 	
-	public static void shuffleMethod(int[] deck) {
-		for(int i = 0; i < deck.length; i++) {
-			Random index = new Random(deck.length);
+	public static int[] shuffleMethod(int[] deck) {
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < deck.length; j++) {
+				int temp = (int)(Math.random() * deck.length);
+				int temp2 = deck[temp];
+				deck[temp] = deck[j];
+				deck[j] = temp2;
+			}
 		}
+		return deck;
 	}
 }
